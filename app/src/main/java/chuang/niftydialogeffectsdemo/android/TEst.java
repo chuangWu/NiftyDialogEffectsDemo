71d75ffbaea10ba9c15f3a3402b52cb4f331029f
wuchuang1992 <812625835@qq.com>
2015-02-12 17:16:02 +0800

    Initial commit

---
+++ app/src/main/java/chuang/niftydialogeffectsdemo/android/TEst.java
@@ -0,0 +1,7 @@
+package chuang.niftydialogeffectsdemo.android;
+
+/**
+ * Created by N-251 on 2015/2/12.
+ */
+public class TEst {
+}
---
+++ app/src/main/java/chuang/niftydialogeffectsdemo/android/widget/Test.java
@@ -0,0 +1,7 @@
+package chuang.niftydialogeffectsdemo.android.widget;
+
+/**
+ * Created by N-251 on 2015/2/12.
+ */
+public class Test {
+}
